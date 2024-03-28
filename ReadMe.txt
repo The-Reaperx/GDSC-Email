a83d8be056972805b18051ee5b5579368eaa0279
The Reaper <umair.boss2004@gmail.com>
2024-03-28 06:54:07 +0300

    Intitialising Repository - Email

---
+++ ReadMe.txt
@@ -0,0 +1,7 @@
+The Basic Files:
+1) confirmedList.csv -> All the emails to be sent will be pasted onto column A.
+2) main.py ->
+    |-.send subject and text : The thumbnail data for the email.
+    |-body parameters: Contents of the Email that can be edited. ( Everything from the Title to the Image)
+
+Installation packages: Redmail and pandas**
---
+++ main.py
@@ -0,0 +1,50 @@
+import pandas as pd
+df =pd.read_csv('confirmedList.csv')
+matrix2 = df[df.columns[0]].to_numpy()
+list1 = matrix2.tolist()
+print("The Selected Emails are: ")
+print(list1)
+
+
+# Email Component ---------------------------------------------
+from redmail import gmail
+with open('template.html', 'r') as template:
+    html_string = template.read()
+
+#Set the Email and password ( VERY IMPORTANT!!!)
+gmail.username = 'umair.boss2003@gmail.com'
+gmail.password = 'xlca dztn sxwm xnzv'
+
+for reciever in list1:
+    gmail.send(
+        #Subject and #Text ( This is the thumbnail text )
+        subject = "Confirmation of Attendance",
+        receivers = reciever,
+        text="for the workshop : leadership skills make sure to make your attendance clear for the upcoming workshop",
+        html= html_string,
+        body_images={
+            'my_image1': 'test1.jpg',
+            'my_image3': 'test3.jpg',
+            #This is the Workshop Image that can be changed below
+            'my_image2': 'image2.jpeg',
+        },
+
+        #Contents of the Email
+        body_params={
+            'title': "Confirmation of Attendance",
+            'subtitle': "Workshop : Leadership Skills",
+            'body_line1': 'We are looking forward to seeing you in the upcoming workshop and learning together.',
+            'body_line2': 'Please confirm your attendance by pressing the button below: ',
+
+            #To Hide the button, set hide_button to 'hidden' and link to an empty String.
+            'hide_button': '',
+            'link': 'https://forms.gle/aTG2ChsSazLwPZpK8'
+
+
+        }
+
+
+
+    )
+
+print("All emails have been sent successfully.")
