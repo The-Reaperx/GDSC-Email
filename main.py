a67b9d8e6a1c6f590e2639f7ec33be68c05c2de9
The Reaper <umair.boss2004@gmail.com>
2024-04-27 15:10:55 +0300

    Email - Version 2

--- main.py
+++ main.py
@@ -12,33 +12,33 @@ with open('template.html', 'r') as template:
     html_string = template.read()
 
 #Set the Email and password ( VERY IMPORTANT!!!)
-gmail.username = ' '
-gmail.password = ' '
+gmail.username = ''
+gmail.password = ''
 
 for reciever in list1:
     gmail.send(
         #Subject and #Text ( This is the thumbnail text )
         subject = "Confirmation of Attendance",
         receivers = reciever,
-        text="for the workshop : leadership skills make sure to make your attendance clear for the upcoming workshop",
+        text="for the Bootcamp : اساسيات البحث العلمي ",
         html= html_string,
         body_images={
             'my_image1': 'test1.jpg',
             'my_image3': 'test3.jpg',
             #This is the Workshop Image that can be changed below
-            'my_image2': 'image2.jpeg',
+            'my_image2': 'image2.jpg',
         },
 
         #Contents of the Email
         body_params={
             'title': "Confirmation of Attendance",
-            'subtitle': "Workshop : Leadership Skills",
-            'body_line1': 'We are looking forward to seeing you in the upcoming workshop and learning together.',
-            'body_line2': 'Please confirm your attendance by pressing the button below: ',
+            'subtitle': "Bootcamp : اساسيات البحث العلمي",
+            'body_line1': 'We are looking forward to seeing you in the upcoming bootcamp.',
+            'body_line2': 'Note: You have 3 hours to fill in the form. Otherwise your seat will be given away.',
 
             #To Hide the button, set hide_button to 'hidden' and link to an empty String.
             'hide_button': '',
-            'link': 'https://forms.gle/aTG2ChsSazLwPZpK8'
+            'link': 'https://shorturl.at/djAH5'
 
 
         }
